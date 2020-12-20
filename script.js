d60041b1d5f9396e602fe6393e6f1a83a5abc81e
Bjørnar Øvrebø <bjornarovrebo@Bjrnars-MacBook-Pro.local>
2020-12-21 00:33:56 +0100

    Delivery

--- script.js
+++ script.js
@@ -1,13 +1,52 @@
 //question1
-
-var petArray = ["cat", "cow", "dog"]
-    console.log(petArray[1]);
+var petArray = ["cat", "cow", "dog"];
 
 //question2
-
-petArray.push("sheep");
-    console.log(petArray.length);
+console.log(petArray[1]);
 
 //question3
+petArray.push("sheep");
+
+//question4
+console.log(petArray.length);
+
+//question5
+var cat = {
+    name: "Bono",
+    colour: "black",
+    age: 6
+};
+
+//question6
+console.log(cat.age);
+
+//question7
+var catArray [
+    {
+        name: "Bono",
+        colour: "black",
+        age: 6
+    }    
+];
+
+//question8
+
+for(var i = 0; i < catArray.length; i++){
+    console.log(catArray[i]);
+};
+
+//question9
+
+function logToConsole(firstFunction) {
+    console.log("firstFunction");
+}
+
+logToConsole("Yes");
+
+//question10
+
+logToConsole ("Donkey");
+
+
 
 
